29ffa7232647a5b8260faff76af6784626b5e4b5
MOIS3Y <stepan@zhukovsky.me>
2023-02-05 15:40:16 +0900

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Home Manager configuration for my Void Linux workstation";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    
+    nixgl = {
+      url = "github:guibou/nixGL";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+
+
+  outputs = { nixpkgs, home-manager, nixgl, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      overlays = [ nixgl.overlay ];
+    in {
+      homeConfigurations.void = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [
+          ./home.nix
+        ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+    };
+}
+
