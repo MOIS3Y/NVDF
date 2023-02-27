798e934039ee8f07f7b22fed41c262cf4820f4dd
MOIS3Y <stepan@zhukovsky.me>
2023-02-28 01:47:06 +0900

    Refactoring flake.nix delete hyprland

--- flake.nix
+++ flake.nix
@@ -2,18 +2,15 @@
   description = "Home Manager configuration for my Void Linux workstation";
 
   inputs = {
-
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixgl.url = "github:guibou/nixGL";
-    hyprland.url = "github:hyprwm/Hyprland";
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, home-manager, nixgl, hyprland, ... }:
+  outputs = { nixpkgs, home-manager, nixgl, ... }:
     let
       # system = "x86_64-linux";
       # pkgs = nixpkgs.legacyPackages.${system};
@@ -24,21 +21,13 @@
       };
     in {
       homeConfigurations = {
-        void = home-manager.lib.homeManagerConfiguration {
+         workstation = home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
-
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
           modules = [ ./home/stepan/workstation ];
-
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
-          extraSpecialArgs = {
-            inherit nixgl;
-          };
+          extraSpecialArgs = { inherit nixgl; };
         };
-      packages.x86_64-linux.nixgl = pkgs.nixgl.auto.nixGLDefault;
       };
+      packages.x86_64-linux.nixgl = pkgs.nixgl.auto.nixGLDefault;
     };
 }
 
