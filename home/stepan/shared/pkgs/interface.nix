38bd9b7698691a95d266adefe06744060331bbe3
MOIS3Y <stepan@zhukovsky.me>
2023-03-01 00:15:36 +0900

    Test add rofi with config files

--- home/stepan/shared/pkgs/interface.nix
+++ home/stepan/shared/pkgs/interface.nix
@@ -7,9 +7,18 @@
     # widgets:
     eww
 
+    #window switcher
+    rofi
+
     # QT5 styles:
     libsForQt5.qt5ct
     libsForQt5.qtstyleplugin-kvantum
   ];
+  home.file.".config/rofi" = {
+      source = ../config/rofi;
+      recursive = true;
+      # onChange = builtins.readFile ../config/rofi/config.rasi;
+      # executable = true;
+    };
 }
 
