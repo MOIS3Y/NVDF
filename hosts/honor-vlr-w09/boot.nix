7c388fb3cc0a98dbbadef0081f43ee713cb3eaff
MOIS3Y <stepan@zhukovsky.me>
2023-03-25 00:56:09 +0900

    Replace grub-theme sha256 hash to sri try again)

--- hosts/honor-vlr-w09/boot.nix
+++ hosts/honor-vlr-w09/boot.nix
@@ -33,7 +33,7 @@
       theme = pkgs.fetchzip {
         # https://github.com/AdisonCavani/distro-grub-themes
         url = https://github.com/AdisonCavani/distro-grub-themes/releases/download/v3.1/huawei.tar;
-        hash = "sha256-e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
+        hash = "sha256-Ak1BMIEwFYGN/ME6nn+nBB1dWJmXN/iYUguVQIEZivA=";
         stripRoot = false;
       };
     };
